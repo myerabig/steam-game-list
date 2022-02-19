cbf16fbbf50a25fd504d148e89b89aee56fbc05a
Abby Myers <myerabig@aol.com>
2022-02-19 11:54:50 -0500

    Takes Steam ID input

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Project Background
+# Steam Game List
+
+## Project Background
 
 This project was created by Abby Myers and Claire Nicholas for Michigan Tech's 2022 Winter Wonderhack.
+
+## FAQ
+
+**Why is nothing working?**
+Download the CORS blocker for Google Chrome with the provided button.
+
+Then, don't forget to turn the extension on.
+
+**Why can't I get my game data?**
+Make sure that your Steam profile privacy settings are set to public, especially the game details.
+
+**How do I get my Steam ID?**
+If you go to your Steam profile in a web browser and see a long number at the end of your URL, that's your ID.
+
+If you have a custom URL, you can use [this site](https://steamid.io/) with your custom URL to get your steamID64.
--- src/App.js
+++ src/App.js
@@ -1,79 +1,113 @@
-import * as React from 'react';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import theme from './theme';
+import React, { useState } from 'react';
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import theme from "./theme";
 
-import './App.css';
-import secret from './Resources/secret.json';
-import steamGameList from './Resources/steamGameList.json';
+import "./App.css";
+import secret from "./Resources/secret.json";
+import steamGameList from "./Resources/steamGameList.json";
 
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 
 function App() {
-	const getData = async () => {
-		let apiKey = secret['steam-api-key'];
-		let userId = secret['my-id'];
+	const [steamUserId, setSteamUserId] = useState("");
 
-		let url =
-			'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' +
-			apiKey +
-			'&steamid=' +
-			userId +
-			'&format=json';
+    const getData = async () => {
+        let apiKey = secret["steam-api-key"];
+        // let userId = secret["my-id"];
 
-		await fetch(url)
-			.then((response) => response.json())
-			.then((json) => {
-				// Combine all games into one array
-				let allGames = [];
-				json.response.games.forEach((gameArr) => {
-					allGames.push(gameArr);
-				});
+        let url =
+            "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
+            apiKey +
+            "&steamid=" +
+            steamUserId +
+            "&format=json";
 
-				// Sort by playtime (highest to lowest)
-				allGames.sort((a, b) => parseFloat(b.playtime_forever) - parseFloat(a.playtime_forever));
+        await fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                // Combine all games into one array
+                let allGames = [];
+                json.response.games.forEach((gameArr) => {
+                    allGames.push(gameArr);
+                });
 
-				let mostPlayed = steamGameList.applist.apps.find((game) => game.appid == allGames[0].appid).name;
+                // Sort by playtime (highest to lowest)
+                allGames.sort(
+                    (a, b) =>
+                        parseFloat(b.playtime_forever) -
+                        parseFloat(a.playtime_forever)
+                );
 
-				console.log(allGames);
-				console.log('Most Played: ' + mostPlayed);
+                let mostPlayed = steamGameList.applist.apps.find(
+                    (game) => game.appid === allGames[0].appid
+                ).name;
 
-				document.getElementById('answer-pic').src =
-					'https://steamcdn-a.akamaihd.net/steam/apps/' + allGames[0].appid + '/library_600x900_2x.jpg';
-				document.getElementById('answer').innerHTML = mostPlayed;
-			})
-			.catch((error) => console.error(error));
-	};
+                document.getElementById("answer-pic").src =
+                    "https://steamcdn-a.akamaihd.net/steam/apps/" +
+                    allGames[0].appid +
+                    "/library_600x900_2x.jpg";
+                document.getElementById("answer").innerHTML = mostPlayed;
+            })
+            .catch((error) => console.error(error));
+    };
 
-	const theme2 = createTheme({
-		palette: {
-		  primary: {
-			main: '#FF0000',
-		  },
-		},
-	  });
+    const theme2 = createTheme({
+        palette: {
+            primary: {
+                main: "#FF0000",
+            },
+        },
+    });
 
-	return (
-		<div className="App">
-			<ThemeProvider theme={theme2}>
-				<Button
-					id="cors-button"
-					color="primary"
-					variant="outlined"
-					target="_blank"
-					href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en"
-				>
-					Download CORS Blocker
-				</Button>
-				<Button id="test-button" variant="contained" color="primary" onClick={getData}>
-					What game has Abby played the most?
-				</Button>
-				<br />
-				<br />
-				<img id="answer-pic" src="" />
-				<h1 id="answer"></h1>
-			</ThemeProvider>
-		</div>
-	);
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme2}>
+                <Grid
+                    container
+                    spacing={4}
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Button
+                            id="cors-button"
+                            color="primary"
+                            variant="outlined"
+                            target="_blank"
+                            href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en"
+                        >
+                            Download CORS Blocker
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            id="steam-id"
+                            label="Steam ID"
+                            variant="outlined"
+							onChange={(e) => setSteamUserId(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            id="test-button"
+                            variant="contained"
+                            color="primary"
+                            onClick={getData}
+                        >
+                            What game have I played the most?
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <img id="answer-pic" src="" />
+                        <h1 id="answer"></h1>
+                    </Grid>
+                </Grid>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
