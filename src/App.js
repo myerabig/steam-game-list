5ccabd06dde29012d004188975dce66b1d97518c
Abby Myers <myerabig@gmail.com>
2022-02-18 22:32:03 -0500

    Got calls to Steam API working

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
 import * as React from 'react';
 
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-
 import './App.css';
+import secret from './Resources/secret.json';
+import steamGameList from './Resources/steamGameList.json';
+
+import Button from '@mui/material/Button';
 
 function App() {
-  const [open, setOpen] = React.useState(false);
+	const getData = async () => {
+		let apiKey = secret['steam-api-key'];
+		let userId = secret['my-id'];
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+		let url =
+			'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' +
+			apiKey +
+			'&steamid=' +
+			userId +
+			'&format=json';
 
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
+		await fetch(url)
+			.then((response) => response.json())
+			.then((json) => {
+				// Combine all games into one array
+				let allGames = [];
+				json.response.games.forEach((gameArr) => {
+					allGames.push(gameArr);
+				});
 
-    setOpen(false);
-  };
+				// Sort by playtime (highest to lowest)
+				allGames.sort((a, b) => parseFloat(b.playtime_forever) - parseFloat(a.playtime_forever));
 
-  const action = (
-    <React.Fragment>
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={handleClose}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </React.Fragment>
-  );
+				let mostPlayed = steamGameList.applist.apps.find((game) => game.appid == allGames[0].appid).name;
 
-  return (
-    <div className="App">
-      <Button onClick={handleClick}>Click Me</Button>
-      <Snackbar
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        message="This is called a snackbar. I'm hungry..."
-        action={action}
-      />
-    </div>
-  );
+				console.log(allGames);
+				console.log('Most Played: ' + mostPlayed);
+
+				document.getElementById('answer-pic').src = 'https://steamcdn-a.akamaihd.net/steam/apps/' + allGames[0].appid +'/library_600x900_2x.jpg';
+				document.getElementById('answer').innerHTML = mostPlayed;
+			})
+			.catch((error) => console.error(error));
+	};
+
+	return (
+		<div className="App">
+			<Button id="test-button" variant="contained" onClick={getData}>
+				What game has Abby played the most?
+			</Button>
+			<br />
+			<br />
+			<img id="answer-pic" src="" />
+			<h1 id="answer"></h1>
+		</div>
+	);
 }
 
 export default App;
