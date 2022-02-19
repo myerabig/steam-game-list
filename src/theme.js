6b44a28c00bbea93e279fd2b6612cd2878d3d974
Claire Nicholas <claire@rover-228-188.rovernet.mtu.edu>
2022-02-18 22:49:56 -0500

    added theme :)

---
+++ src/theme.js
@@ -0,0 +1,36 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#0D2840',
+    },
+    secondary: {
+      main: '#165F8C',
+    },
+    text: {
+      primary: '#0A1926',
+      secondary: '#0F2859',
+      disabled: '#145E8C',
+      hint: '#127CA6',
+    },
+    warning: {
+      main: '#F9912B',
+    },
+    error: {
+      main: '#F46161',
+    },
+    success: {
+      main: '#049DD9',
+    },
+    divider: '#383F40',
+    info: {
+      main: '#B2D6ED',
+    },
+  },
+  typography: {
+    fontSize: 14,
+    htmlFontSize: 17,
+  },
+};
